fc91463b95b240e669e5fa133f3c3ff89ed2c328
saladthieves <saladthieves@gmail.com>
2020-10-01 17:49:09 +0200

    Initial commit

---
+++ app/src/main/java/com/gebeya/hisab/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gebeya.hisab
+
+import android.os.Bundle
+import com.gebeya.hisab.framework.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
