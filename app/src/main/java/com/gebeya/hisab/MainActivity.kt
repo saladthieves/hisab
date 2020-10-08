5685b8c5ede7cce8ae5e128d1084d538c64b6c01
saladthieves <saladthieves@gmail.com>
2020-10-08 18:31:38 +0200

    SeekBar with Custom Step

--- app/src/main/java/com/gebeya/hisab/MainActivity.kt
+++ app/src/main/java/com/gebeya/hisab/MainActivity.kt
@@ -2,17 +2,22 @@ package com.gebeya.hisab
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.CheckBox
 import android.widget.CompoundButton
+import android.widget.SeekBar
 import com.gebeya.hisab.data.Controller
 import com.gebeya.hisab.data.food.FoodType
+import com.gebeya.hisab.data.meal.MealType
 import com.gebeya.hisab.framework.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity(), CompoundButton.OnCheckedChangeListener {
+class MainActivity : BaseActivity(), CompoundButton.OnCheckedChangeListener,
+    AdapterView.OnItemSelectedListener {
 
     private lateinit var fetiraCheckBox: CheckBox
     private val controller = Controller()
+    val step = 15
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,24 @@ class MainActivity : BaseActivity(), CompoundButton.OnCheckedChangeListener {
         fulMedamesCheck.setOnCheckedChangeListener(this)
         chechebsaCheck.setOnCheckedChangeListener(this)
         tibsCheck.setOnCheckedChangeListener(this)
+
+        mealTypeSpinner.onItemSelectedListener = this
+        val listener = object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val final = progress * step
+                totalPeopleLabel.text = "Total people: $final"
+                controller.addPeople(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        }
+        totalPeopleSeekBar.setOnSeekBarChangeListener(listener)
     }
 
     override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
@@ -44,4 +67,11 @@ class MainActivity : BaseActivity(), CompoundButton.OnCheckedChangeListener {
             controller.removeFood(type)
         }
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val type = MealType.values()[position]
+        controller.addMealType(type)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) = Unit
 }
--- app/src/main/java/com/gebeya/hisab/data/controller.kt
+++ app/src/main/java/com/gebeya/hisab/data/controller.kt
@@ -2,10 +2,14 @@ package com.gebeya.hisab.data
 
 import com.gebeya.hisab.data.food.Food
 import com.gebeya.hisab.data.food.FoodType
+import com.gebeya.hisab.data.meal.Meal
+import com.gebeya.hisab.data.meal.MealType
 import com.gebeya.hisab.framework.util.logD
 
 class Controller(
-    val foods: MutableList<Food> = mutableListOf()
+    val foods: MutableList<Food> = mutableListOf(),
+    var meal: Meal = Meal(MealType.Breakfast),
+    var peopleDiscount: Double = 0.0
 ) {
     fun addFood(type: FoodType) {
         val food = Food(type)
@@ -23,8 +27,24 @@ class Controller(
         log()
     }
 
+    fun addMealType(type: MealType) {
+        meal = Meal(type)
+        log()
+    }
+
+    fun addPeople(people: Int) {
+        peopleDiscount = when(people) {
+            in 0..4 -> 0.0
+            in 5..14 -> 0.3
+            else -> 0.5
+        }
+        log()
+    }
+
     private fun log() {
         d("Foods: $foods")
+        d("Meal: $meal")
+        d("People discount: $peopleDiscount")
     }
 
     private fun d(message: String) {
