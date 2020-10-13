534fe4520e32c3a297b52cb4c4dfeeb0883bd6b4
saladthieves <saladthieves@gmail.com>
2020-10-13 16:58:12 +0200

    Start for class

---
+++ app/src/main/java/com/gebeya/hisab/SummaryActivity.kt
@@ -0,0 +1,11 @@
+package com.gebeya.hisab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SummaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary)
+    }
+}
